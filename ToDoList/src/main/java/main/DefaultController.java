2877c616603cafd0ede9b929f0fd562ea2ab6195
roman883 <s.rb@list.ru>
2020-02-14 11:10:43 +0900

    ToDoList: TaskServive class created, TaskController Class modified

--- ToDoList/src/main/java/main/DefaultController.java
+++ ToDoList/src/main/java/main/DefaultController.java
@@ -6,10 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 @Controller
 public class DefaultController {
--- ToDoList/src/main/java/main/TaskController.java
+++ ToDoList/src/main/java/main/TaskController.java
@@ -1,65 +1,44 @@
 package main;
 
-import main.model.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import main.model.Task;
-
-import java.util.ArrayList;
-import java.util.List;
+import service.RepositoryService;
 
 @RestController
+@ComponentScan("service")
 public class TaskController {
 
+    private final RepositoryService repositoryService;
+
     @Autowired
-    private TaskRepository taskRepository;
+    public TaskController (RepositoryService repositoryService) {
+        this.repositoryService = repositoryService;
+    }
 
     @GetMapping(value = "/tasks/") // @RequestMapping(value = "/tasks/", method = RequestMethod.GET)
-    public List<Task> list() {
-        ArrayList<Task> tasks = new ArrayList<>();
-        taskRepository.findAll().forEach(tasks::add);
-        return tasks;
+    public ResponseEntity list() {
+        return repositoryService.getAllTasks();
     }
 
     @PostMapping(value = "/tasks/")
-    public int add(Task task) {
-        Task tempTask;
-        synchronized (taskRepository) {
-            tempTask = taskRepository.save(task);
-        }
-        return tempTask.getId();
+    public ResponseEntity add(Task task) {
+        return repositoryService.addTask(task);
     }
 
     @GetMapping("/tasks/{id}")
     public ResponseEntity get(@PathVariable int id) {
-        try {
-            Task task = taskRepository.findById(id).orElseThrow(IllegalArgumentException::new);
-            return new ResponseEntity(task, HttpStatus.OK);
-        } catch (IllegalArgumentException ex) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
+       return repositoryService.getTask(id);
     }
 
     @PutMapping(value = "/tasks/{id}", params = {"name", "description"})
-    public @ResponseBody
-    ResponseEntity put(
+    public @ResponseBody ResponseEntity put(
             @PathVariable int id,
             @RequestParam(value = "name") String name,
             @RequestParam(value = "description") String description) {
-        try {
-            Task task;
-            synchronized (taskRepository) {
-                task = taskRepository.findById(id).orElseThrow(IllegalArgumentException::new);
-                task.setDescription(description);
-                task.setName(name);
-                taskRepository.save(task);
-            }
-            return new ResponseEntity(task, HttpStatus.OK);
-        } catch (IllegalArgumentException ex) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
+            return repositoryService.putTask(id, name, description);
     }
 
     @PatchMapping(value = "/tasks/{id}", params = {"name", "description"})
@@ -68,32 +47,11 @@ public class TaskController {
             @PathVariable int id,
             @RequestParam(value = "name") String name,
             @RequestParam(value = "description") String description) {
-        try {
-            Task task;
-            synchronized (taskRepository) {
-                task = taskRepository.findById(id).orElseThrow(IllegalArgumentException::new);
-                if (!description.equals("")) {
-                    task.setDescription(description);
-                }
-                if (!name.equals("")) {
-                    task.setName(name);
-                }
-                taskRepository.save(task);
-            }
-            return new ResponseEntity(task, HttpStatus.OK);
-        } catch (IllegalArgumentException ex) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
+        return repositoryService.patchTask(id, name, description);
     }
 
     @DeleteMapping("/tasks/{id}")
     public ResponseEntity delete(@PathVariable int id) {
-        synchronized (taskRepository) {
-            if (taskRepository.existsById(id)) {
-                taskRepository.deleteById(id);
-                return ResponseEntity.status(HttpStatus.OK).body(null);
-            }
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        return repositoryService.deleteTask(id);
     }
 }
