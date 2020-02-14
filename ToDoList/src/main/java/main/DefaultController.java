9dc5a9c30afddac03a125895ff2d3a779e1d16a4
roman883 <s.rb@list.ru>
2020-02-14 11:54:58 +0900

    JAR creation enabled, RepositoryService class added. Two branches merged

--- ToDoList/src/main/java/main/DefaultController.java
+++ ToDoList/src/main/java/main/DefaultController.java
@@ -3,7 +3,7 @@ package main;
 import main.model.Task;
 import main.model.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +13,8 @@ import java.util.ArrayList;
 @Controller
 public class DefaultController {
 
-    @Value("${someParameter.value}")
-    private String someParameter;
+//    @Value("${someParameter.value}")
+//    private String someParameter;
 
     @Autowired
     TaskRepository taskRepository;
