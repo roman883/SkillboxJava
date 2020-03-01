5be159b1b9b64333598b9f4b9126b665a4b4efc6
roman883 <s.rb@list.ru>
2020-03-01 09:40:00 +0900

    Minor changes to HDFS example. Everything works fine, including append strings to file function

--- 15_BigData/HDFSExample/src/main/java/Main.java
+++ 15_BigData/HDFSExample/src/main/java/Main.java
@@ -6,41 +6,43 @@ public class Main
     {
         System.setProperty("HADOOP_USER_NAME", "root");
 
-        FileAccess fileAccess = new FileAccess("hdfs://119f0f2a516c:8020");
-        String pathDirectoryCreate = "test902/56/";
-        String path2 = "test901";
-        String path3 = "test902/fileTest2.txt";
-        fileAccess.create(pathDirectoryCreate);
-        fileAccess.create(path2);
-        fileAccess.create(path3);
-        fileAccess.create("test902/00/fileTest3.txt");
+        FileAccess fileAccess = new FileAccess("hdfs://540cc67e0fa6:8020");
+        String pathDir1 = "test001/01/";
+        String pathDir2 = "test002";
+        String pathFile1 = "test002/fileTest1.txt";
+        String pathFile2 = "test002/internal/fileTest2.txt";
+        fileAccess.create(pathDir1);
+        fileAccess.create(pathDir2);
+        fileAccess.create(pathFile1);
+        fileAccess.create(pathFile2);
 
-//        fileAccess.delete(pathDirectoryCreate);
         String content = getRandomWord();
         System.out.println("=======================");
         System.out.println(content);
         System.out.println("======================");
-        fileAccess.append(path3, content);
+        fileAccess.append(pathFile2, content);
 
-        System.out.println("\nЧитаем файл");
-        System.out.println(fileAccess.read(path3));
-        fileAccess.read(path2);
-        if (fileAccess.isDirectory(path3)) {
-            System.out.println(path3 + " это папка");
+        System.out.println("\nЧитаем файл:");
+        System.out.println(fileAccess.read(pathFile2));
+        fileAccess.read(pathFile2);
+        System.out.println("\nПроверка является ли объект папкой/файлом:");
+        if (fileAccess.isDirectory(pathDir2)) {
+            System.out.println("\t" + pathDir2 + " - папка");
         } else {
-            System.out.println(path3 + " это файл");
+            System.out.println("\t" + pathDir2 + " - файл");
         }
-        if (fileAccess.isDirectory(path2)) {
-            System.out.println(path2 + " это папка");
+        if (fileAccess.isDirectory(pathFile1)) {
+            System.out.println("\t" + pathFile1 + " - папка");
         } else {
-            System.out.println(path2 + " это файл");
+            System.out.println("\t" + pathFile1 + " - файл");
         }
-        System.out.println("Выводим все файлы: ");
-        fileAccess.list("test902").forEach(System.out::println);
-
-        fileAccess.delete(pathDirectoryCreate);
-        fileAccess.delete(path2);
-        fileAccess.delete(path3);
+        System.out.println("\nВыводим все файлы и папки: ");
+        fileAccess.list(pathDir2).forEach(System.out::println);
+        System.out.println("\nУдаляем все файлы и папки:");
+        fileAccess.delete(pathFile1);
+        fileAccess.delete(pathFile2);
+        fileAccess.delete(pathDir1);
+        fileAccess.delete(pathDir2);
         fileAccess.hdfs.close();
     }
 
